37eb7b32e054a8fd9f37c1c4c4b68ec13ff0a49f
Kaleidoscopee <romanj1997@gmail.com>
2024-11-16 13:59:17 -0500

    alternativeflavors now prints

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,7 +1,9 @@
 const names = require("./04-names.js");
 const hello = require("./05-utils.js");
-const alternativeFlavor = require("./06-alternative-flavor.js");
+const cakeData = require("./06-alternative-flavor.js");
 const mindGrenade = require("./07-mind-grenade.js");
 
 
 hello(names.kate);
+
+console.log(cakeData);
